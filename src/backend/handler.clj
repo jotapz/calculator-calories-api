257991b35e381bd659689d1d16a3572c6b0065ee
João Pedro <jpedros1511@gmail.com>
2026-06-13 19:57:25 -0300

    subi errado

--- src/backend/handler.clj
+++ src/backend/handler.clj
@@ -7,35 +7,7 @@
             [clj-http.client :as client]
             [backend.apiIgnore :as api]
             [backend.db :as db]))
-
-(def usda-key "E8YESy0vfvPpVxhFfhQIfYgok5Kkl1yeil5yT9cy")
-
-(defn buscar-calorias-alimento [nome quantidade]
-  (let [busca     (client/get "https://api.nal.usda.gov/fdc/v1/foods/search"
-                              {:query-params {:query    nome
-                                             :pageSize 1
-                                             :dataType "Foundation,SR Legacy"
-                                             :api_key  usda-key}
-                               :as :json})
-        alimento  (first (get-in busca [:body :foods]))
-        nutriente (first (filter #(= (:nutrientId %) 1008)
-                                 (:foodNutrients alimento)))
-        calorias-por-100g (or (:value nutriente) 0)]
-    (* calorias-por-100g (/ quantidade 100.0))))
-
-(def api-key "FPMcg4KM6VTDqQMW8uHSYrATmbnNo4UEFVuohsHR")
-(def headers {"X-Api-Key" api-key})
-
-(defn buscar-calorias-atividade [nome duracao-minutos]
-  (let [resposta (client/get "https://api.api-ninjas.com/v1/caloriesburned"
-                             {:headers      headers
-                              :query-params {:activity nome}
-                              :as           :json})
-        itens    (:body resposta)]
-    (if (empty? itens)
-      0
-      (let [calorias-por-hora (:calories_per_hour (first itens))]
-        (* (/ calorias-por-hora 60.0) duracao-minutos)))))
+            
 
 (defn jsonn
   ([retorno]
